44525238ecbe018a1659236dfdf1a8b0b570bd55
karol.koltun <karol.koltun@britenet.com.pl>
2018-04-26 15:32:06 +0200

    Initial commit

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,31 @@
+package app;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class Application {
+
+    public static void main(String args[]) throws Exception {
+        final CamelContext camelContext = new DefaultCamelContext();
+
+        RouteBuilder ftpRoute = new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("ftp://user_ftp@localhost:21?binary=true&password=password&stepwise=true&streamDownload=true")
+                    .process((Exchange e) -> {
+                        System.out.println("Downloading started");
+                    })
+                    .to("file://download/")
+                    .process((Exchange e) -> {
+                        System.out.println("Passed to file");
+                    });
+                }
+        };
+
+        camelContext.addRoutes(ftpRoute);
+        camelContext.start();
+        Thread.sleep(1000000000);
+    }
+}
